584a8fb57863e7af322d27294f8c33dbd920c18a
nusantararegas <it.supportnr@pertamina.com>
2025-06-06 13:59:59 +0700

    update vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,25 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
+	output: "static",
 	integrations: [tailwind()],
 	vite: {
 		envPrefix: ["SANITY_"],
 		envDir: ".",
+		build: {
+			chunkSizeWarningLimit: 6000,
+			rollupOptions: {
+				output: {
+					manualChunks: (id) => {
+						if (id.includes('@mlc-ai/web-llm')) {
+							return 'webllm';
+						}
+						if (id.includes('node_modules')) {
+							return 'vendor';
+						}
+					},
+				},
+			},
+		},
 	},
 });
